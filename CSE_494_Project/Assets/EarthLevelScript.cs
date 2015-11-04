1688a5b1115f2515529127d33095af21f6eaebf9
Eric Tang <Erictangis@gmail.com>
2015-11-03 19:16:33 -0700

    Updated Dialogue

--- CSE_494_Project/Assets/EarthLevelScript.cs
+++ CSE_494_Project/Assets/EarthLevelScript.cs
@@ -11,15 +11,19 @@ public class EarthLevelScript : MonoBehaviour {
     public GameObject QuestButton;
     public GameObject InfoPanel;
     public GameObject InfoButton;
+    public string[] NPCTextPanels;
     public string[] textPanels;
+    public string[] NPCCollectedMineraltextPanels;
     public string[] CollectedMineraltextPanels;
     Text textPanelText;
+    Text NPCTalkingTitle;
     int sizeOfTextPanels;
     int currentPanel = 0;
     
 	// Use this for initialization
 	void Start () {
         textPanelText = DialogPanel.transform.GetChild(1).GetComponent<Text>();
+        NPCTalkingTitle = DialogPanel.transform.GetChild(0).GetComponent<Text>();
         sizeOfTextPanels = textPanels.Length;
     }
 	
@@ -28,8 +32,9 @@ public class EarthLevelScript : MonoBehaviour {
         if (PlayerPrefs.HasKey("hasEarthinite"))
         {
             textPanelText.text = CollectedMineraltextPanels[currentPanel];
+            NPCTalkingTitle.text = NPCCollectedMineraltextPanels[currentPanel];
             //Finished second dialog
-            if(currentPanel >= sizeOfTextPanels - 1)
+            if (currentPanel >= sizeOfTextPanels - 1)
             {
                 //Enable trigger for spaceship to load next level
                 player.GetComponent<QuestManager>().Spaceship.GetComponent<CapsuleCollider>().enabled = true;
@@ -38,6 +43,7 @@ public class EarthLevelScript : MonoBehaviour {
         else
         {
             textPanelText.text = textPanels[currentPanel];
+            NPCTalkingTitle.text = NPCTextPanels[currentPanel];
         }
 	}
 
