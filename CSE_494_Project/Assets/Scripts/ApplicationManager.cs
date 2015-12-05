2bb9cd2393604269ea52e81c804eec1630e180a2
Eric Tang <Erictangis@gmail.com>
2015-12-05 12:01:04 -0700

    Final Touches

--- CSE_494_Project/Assets/Scripts/ApplicationManager.cs
+++ CSE_494_Project/Assets/Scripts/ApplicationManager.cs
@@ -10,10 +10,6 @@ public class ApplicationManager : MonoBehaviour {
 
 	public void Quit () 
 	{
-		#if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
-		#else
 		Application.Quit();
-		#endif
 	}
 }
